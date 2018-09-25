cb89410a874d73753d9830ca183278521bbe56b6
zywaiting <1573240324@qq.com>
2018-09-25 22:39:14 +0800

    ...

---
+++ src/test/java/com/github/zywaiting/httputil/HttputilApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.zywaiting.httputil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttputilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
